75f1c062c8ff46657a05e6eea56542579ef770de
abhi sharma <abhisharma968089@gmail.com>
2026-03-14 23:21:32 +0530

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,36 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://certfiner.vercel.app"
+
+  return [
+    {
+      url: `${baseUrl}/`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/certificates`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/privacy-policy`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/terms`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/disclaimer`,
+      lastModified: new Date(),
+    }
+  ]
+}

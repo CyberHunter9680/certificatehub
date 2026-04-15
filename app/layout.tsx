1791aa87ea1a46a3917033500246e6863ab8d8ed
abhi sharma <abhisharma968089@gmail.com>
2026-04-15 12:38:54 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,13 +46,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <head><script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "wbyghr9uyj");
-</script></head>
       <body
         className={`${poppins.variable} antialiased bg-[#0e0e1a] text-white min-h-screen flex flex-col`}
       >
